f6a4b0643c6134e5650ddb09edc05d53443f1e99
Aga Chimdesa <139769747+achimdesa@users.noreply.github.com>
2026-05-28 08:49:27 +0300

    Delete  eter-events directory
    Delete the duplicated eter-events directory

--- eter-events/js/supabase.js
+++
@@ -1,7 +0,0 @@
-const supabaseUrl = 'https://ameswctizbwbkcclkvve.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtZXN3Y3RpemJ3YmtjY2xrdnZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3ODcxMjMsImV4cCI6MjA5NTM2MzEyM30.iGj2QcKa70IeANK-tjh_bKtlhFwtgot267HrIAvraW8';
-
-const supabaseClient = supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
